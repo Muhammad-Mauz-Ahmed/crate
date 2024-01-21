89a9f6494b2214d4147d96d25af22a171116d894
Muhammad-Mauz-Ahmed <154886680+Muhammad-Mauz-Ahmed@users.noreply.github.com>
2024-01-21 15:56:03 +0500

    Add files via upload

---
+++ SeoTags.Sample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeoTags.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //HttpContext.SetSeoInfo(seoInfo =>
+            //{
+            //    //Set seo info
+            //});
+            return View();
+        }
+
+        public IActionResult JsonLd1()
+        {
+            return View();
+        }
+
+        public IActionResult JsonLd2()
+        {
+            return View();
+        }
+    }
+}
